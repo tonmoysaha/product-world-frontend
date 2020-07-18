2e9daacfff0c7b82220858ffbf70cb6a0989801a
tonmoysaha <tonmoysahaopi@gmail.com>
2020-07-17 21:43:48 -0700

    implement cart details

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CartItem} from '../../common/cart-item';
+import {CartitemService} from '../../service/cartitem.service';
 
 @Component({
   selector: 'app-cart-details',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartDetailsComponent implements OnInit {
 
-  constructor() { }
+  cartItems: CartItem[] = [];
+  totallPrice: number = 0.0;
+  toallQuantity: number = 0.0;
+
+  constructor(private cartService: CartitemService) { }
 
   ngOnInit(): void {
+    this.listCartDetails();
   }
 
+  private listCartDetails() {
+
+    this.cartItems = this.cartService.cartItems;
+
+    this.cartService.toatallPrice.subscribe( data => this.totallPrice = data);
+
+    this.cartService.totallQuantity.subscribe(data => this.toallQuantity = data);
+  }
 }
