ad7103cb557ca4dd36f53c6420e5370825a09c57
tonmoysaha <tonmoysahaopi@gmail.com>
2020-07-21 01:07:14 -0700

    update checkout

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {CartitemService} from '../../service/cartitem.service';
 
 @Component({
   selector: 'app-checkout',
@@ -8,9 +9,11 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 })
 export class CheckoutComponent implements OnInit {
 
+  totallprice: number = 0.0;
+  totallQuantity: number = 0;
   checkOutFormBuilder: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder , private cartService: CartitemService) { }
 
   ngOnInit(): void {
 
@@ -48,7 +51,23 @@ export class CheckoutComponent implements OnInit {
 
     });
 
+    this.updateTotall()
+
   }
+
+  shippingAddressToBillingAddress(event) {
+    if (event.target.checked) {
+      this.checkOutFormBuilder.controls.billingAddress.setValue(this.checkOutFormBuilder.controls.shippingAddress.value);
+    }else {
+      this.checkOutFormBuilder.controls.billingAddress.reset();
+    }
+  }
+
+  updateTotall(){
+    this.cartService.toatallPrice.subscribe(data => this.totallprice = data);
+    this.cartService.totallQuantity.subscribe(data => this.totallQuantity = data);
+  }
+
 
   onSubmit() {
     console.log(`handling form submission`);
@@ -57,4 +76,5 @@ export class CheckoutComponent implements OnInit {
     console.log(this.checkOutFormBuilder.get('billingAddress').value);
     console.log(this.checkOutFormBuilder.get('creditCard').value);
   }
+
 }
