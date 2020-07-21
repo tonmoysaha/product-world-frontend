fcd96566b7642830d66f600e48318785340e69b4
tonmoysaha <tonmoysahaopi@gmail.com>
2020-07-20 21:57:08 -0700

    checkout component implement for form builder

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  checkOutFormBuilder: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.checkOutFormBuilder = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      })
+    });
+
   }
 
+  onSubmit() {
+
+  }
 }
