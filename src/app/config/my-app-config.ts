fc888ef3db8591fbe952885bf8b305b49c8b96da
tonmoysaha <tonmoysahaopi@gmail.com>
2020-12-27 00:12:16 -0800

    add okta

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+  oidc: {
+    clientId: '0oa36dpnjWIhasrqG5d6',
+    issuer: 'https://dev-2866387.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
